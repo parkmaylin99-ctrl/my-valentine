24ff94e8f5e7cc949f383f8d2a5a9d517be8c88f
meka <parkmaylin99@gmail.com>
2026-02-14 09:12:25 +0600

    init

---
+++ my-valentine-for-e/src/components/SpotifyPlayer/SpotifyPlayer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const SpotifyPlayer = () => {
+  return (
+    <div style={{ marginTop: '20px', width: '100%', maxWidth: '400px' }}>
+      <iframe
+      
+  className="music"
+        style={{ borderRadius: '15px'}}
+        src="https://open.spotify.com/embed/track/1oAwsWBovWRIp7qLMGPIet?utm_source=generator&theme=0" // Замени на ссылку своей песни
+        width="100%"
+        height="152"
+        frameBorder="none"
+        allowFullScreen=""
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+        title="Favorite Song"
+      ></iframe>
+    </div>
+  );
+};
+
+export default SpotifyPlayer;
